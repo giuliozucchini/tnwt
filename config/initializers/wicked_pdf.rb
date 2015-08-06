e33d9d8a706c3db8b910d332aa8cb032e2610836
Giulio Zucchini <giulio.zucchini@gmail.com>
2015-08-06 15:10:27 +0200

    fixed wickedpdf config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,9 +1,9 @@
-WickedPdf.config = {
+WickedPdf.config do |config|
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  if Rails.env.production?
-  	:exe_path => '/app/vendor/bundle/bin/wkhtmltopdf'
+  if Rails.env == 'production' then
+  	config.exe_path = '/app/vendor/bundle/bin/wkhtmltopdf'
   else
-  	:exe_path => '/Users/giuliozucchini/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf'
+  	config.exe_path = '/Users/giuliozucchini/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf'
   end
-}
+end

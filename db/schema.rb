0cbe3879a6d108ad1830584c14a785c7e8bd6fa7
Giulio Zucchini <giulio.zucchini@gmail.com>
2015-08-06 14:59:09 +0200

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150803103646) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "headline"
@@ -27,9 +30,9 @@ ActiveRecord::Schema.define(version: 20150803103646) do
   end
 
   create_table "reports", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
     t.integer  "user_id"
   end
 
@@ -49,7 +52,8 @@ ActiveRecord::Schema.define(version: 20150803103646) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "articles", "reports"
 end

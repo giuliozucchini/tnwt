6b91e77e7c26b72d36ce899ee1913bf6563c91e9
Giulio Zucchini <giulio.zucchini@gmail.com>
2015-08-06 15:06:53 +0200

    fixed pdf gem

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,9 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '/Users/giuliozucchini/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf'
+  if Rails.env.production?
+  	:exe_path => '/app/vendor/bundle/bin/wkhtmltopdf'
+  else
+  	:exe_path => '/Users/giuliozucchini/.rvm/gems/ruby-2.2.1/bin/wkhtmltopdf'
+  end
 }
